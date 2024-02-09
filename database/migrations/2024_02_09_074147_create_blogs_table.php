f8b228c3c16a829cb618047ef0dd1964acdd3fe0
“Niel <niel.daculan@gmail.com>
2024-02-09 16:22:20 +0800

    first migration commit

--- database/migrations/2024_02_09_074147_create_blogs_table.php
+++ database/migrations/2024_02_09_074147_create_blogs_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('user_type', array('admin', 'customer'))->default('customer'); 
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->string('blog_name');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blogs');
     }
 };
